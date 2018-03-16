009aea4babc0e774259c7627fbe1684b29e81c71
kimjeyoung <kimjeyoung1@naver.com>
2018-03-16 19:30:23 +0900

    이전 내용 정리

--- paper/CharCnn/config.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-tf.app.flags.DEFINE_string("train_dir", "./dataset/TREC_QA/trec_train.csv", "trec-qa train dataset dir")
-tf.app.flags.DEFINE_string("test_dir", "./dataset/TREC_QA/trec_test.csv", "trec-qa test dataset dir")
-tf.app.flags.DEFINE_string("model_save", "./model/", "checkpoint dir")
-tf.app.flags.DEFINE_string("checkpoint", "./model/model.ckpt", 'checkpoint')
-
-FLAGS = tf.app.flags.FLAGS
--- paper/classification/FASTTEXT_2016/Bag_of_Tricks_for_Efficient_Text_Classification.md
+++
@@ -1,6 +0,0 @@
-## Bag of Tricks for Efficient Text Classification
-
-
-왕과 매닝 (Wang and Manning, 2012)과 마찬가지로 우리의 동기는 감독되지 않은 단어 표현을 학습하는 데 사용 된 모델에서 영감을 얻은 간단한 기준선을 탐구하는 것입니다. 
-그러니깐 Word Vector Representation을 활용해서 비감독 학습으로 데이터를 수집하는 것?
-
